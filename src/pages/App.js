292fa8b362d1e186f508a22387b48c37dd186e88
zxalfred <zxsprout@foxmail.com>
2019-07-01 17:09:57 +0800

    install router

---
+++ src/pages/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+import Home from './home';
+
+class App extends Component {
+  render() {
+    return (
+      <Home />
+    );
+  }
+}
+
+export default App;
