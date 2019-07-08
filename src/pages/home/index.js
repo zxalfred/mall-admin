4b3f445e77a14c131b8fbdb8519f9577a1af7ac6
zxalfred <zxsprout@foxmail.com>
2019-07-08 11:09:03 +0800

    (FE) 通用

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './index.css';
 
+import PageTitle from '@/components/page-title';
+
 class Home extends Component {
   render() {
     return (
       <div id="page-wrapper">
-        <button className="btn btn-default">test</button>
+        <PageTitle title='首页' />
+        <div className="row">
+          <div className="col-md-12">
+            body
+          </div>
+        </div>
       </div>
     );
   }
